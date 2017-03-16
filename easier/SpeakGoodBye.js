3520a6efb3bd6075513b9242db10687429fa8e0e
Stephanie Metayer <stephmeta@yahoo.com>
2017-03-16 17:56:13 -0400

    JS Code

--- easier/SpeakGoodBye.js
+++ easier/SpeakGoodBye.js
@@ -1,4 +1,3 @@
-
 (function (window) {
 // NOTE! The steps in this file are basically identical to the ones you
 // performed in the SpeakHello.js file.
@@ -10,7 +9,7 @@
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
- var byeSpeaker ={};
+var byeSpeaker = {};
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
 var speakWord = "Good Bye";
@@ -25,4 +24,5 @@ byeSpeaker.speak = function (name) {
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
 window.byeSpeaker = byeSpeaker;
-}) (window);
+
+})(window);
